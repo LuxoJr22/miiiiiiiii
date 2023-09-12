996a061666283382847a588f0e8efa67989497ba
stelviaa <forestiesacha@gmail.com>
2023-09-12 15:44:20 +0200

    verif redir

--- srcs/parsing_redirec.c
+++ srcs/parsing_redirec.c
@@ -6,7 +6,7 @@
 /*   By: sforesti <sforesti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 08:15:45 by sforesti          #+#    #+#             */
-/*   Updated: 2023/09/11 19:09:30 by sforesti         ###   ########.fr       */
+/*   Updated: 2023/09/12 15:44:00 by sforesti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,23 +50,56 @@ t_file	*init_tfile(char *line)
 	file->next = 0;
 	return (file);
 }
-
-void	call_parsing_redir(t_cmd *cmd, char *lines, char *line)
+int verif_redir(t_cmd *cmd)
 {
-	char **tmp;
-	int i;
+	char	**tmp;
+	char	*lines;
+	int		i;
 
 	i = 0;
 	lines = ft_strjoin_f(cmd->arg[0], "\0", 4);
 	while (cmd->arg[++i])
 		lines = ft_strjoin_f(lines, cmd->arg[i], 1);
 	tmp = ft_split_two(lines, " <>");
+	free (lines);
 	if (!tmp[0])
 	{
 		ft_putstr_fd("Minishell: syntax error near unexpected token `newline'\n", 2);
 		cmd->here_doc = 1;
-		return ;
+		free_dptr (tmp);
+		return (1);\
 	}
+	free_dptr (tmp);
+	return (0);
+}
+
+int verif_redir(t_cmd *cmd)
+{
+	char	**tmp;
+	char	*lines;
+	int		i;
+
+	i = 0;
+	lines = ft_strjoin_f(cmd->arg[0], "\0", 4);
+	while (cmd->arg[++i])
+		lines = ft_strjoin_f(lines, cmd->arg[i], 1);
+	tmp = ft_split_two(lines, " <>");
+	free (lines);
+	if (!tmp[0])
+	{
+		ft_putstr_fd("Minishell: syntax error near unexpected token `newline'\n", 2);
+		cmd->here_doc = 1;
+		free_dptr (tmp);
+		return (1);\
+	}
+	free_dptr (tmp);
+	return (0);
+}
+
+void	call_parsing_redir(t_cmd *cmd, char *lines, char *line)
+{
+	if (verif_redir(cmd))
+		return ;
 	if (find_name(cmd->arg, 1) != -1 || find_name(cmd->arg, 2) != -1)
 	{
 		manage_redirec(cmd, line);
